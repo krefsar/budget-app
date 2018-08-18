e4e62826234220999f93e3d94ac86cbc86e58dac
Evan Martinez <evanm.design@gmail.com>
2018-08-18 16:05:51 -0400

    fix text colors

--- mirage/fixtures/budgets.js
+++ mirage/fixtures/budgets.js
@@ -1,5 +1,5 @@
 export default [
-  { id: 8, name: 'Groceries', percentage: 0.06, budget: 0, remaining: 0 },
+  { id: 8, name: 'Groceries', percentage: 0.06, budget: 0, remaining: 100 },
   { id: 2, name: 'Eating out', percentage: 0.05, budget: 0, remaining: 0 },
   { id: 3, name: 'Savings account', percentage: 0.10, budget: 0, remaining: 0 },
   { id: 4, name: 'Personal needs', percentage: 0.05, budget: 0, remaining: 0 },
