0ce5d6a893af72b14a931be11d436362a89d5419
Evan Martinez <evanm.design@gmail.com>
2018-08-19 17:12:43 -0400

    add budget remaining to overview

--- app/controllers/overview.js
+++ app/controllers/overview.js
@@ -2,5 +2,12 @@ import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 
 export default Controller.extend({
-  user: computed.alias('model')
+  user: computed.alias('model.user'),
+  budgets: computed.alias('model.budgets'),
+
+  budgetBalance: computed('budgets.@each.amount', function() {
+    return this.get('budgets').reduce((count, budget) => {
+      return count + budget.get('budget');
+    }, 0);
+  })
 });
--- app/routes/overview.js
+++ app/routes/overview.js
@@ -1,7 +1,11 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
   model() {
-    return this.store.findRecord('user', 1);
+    return RSVP.hash({
+      user: this.store.findRecord('user', 1),
+      budgets: this.store.findAll('budget')
+    });
   }
 });
